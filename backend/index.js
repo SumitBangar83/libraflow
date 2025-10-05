7a3328d84cceefee250ea3981aed39b7fd6eb476
SumitBangar83 <sumitbangar.pc@gmail.com>
2025-10-05 19:32:51 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,16 @@ import authRoutes from './routes/AuthRoutes.js';
 import attendanceRoutes from './routes/AttendanceRoutes.js';
 import userRoutes from './routes/UserRoutes.js';
 import http from 'http'
-import {Server} from 'socket.io';
-const app =  express()
+import { Server } from 'socket.io';
+const app = express()
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "https://adduceable-charissa-communistic.ngrok-free.dev",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
@@ -23,21 +29,21 @@ app.use(bodyParser.json())
 app.use(cors());
 dotenv.config();
 app.use((req, res, next) => {
-    req.io = io;
-    next(); // Agle step par jaane ke liye 'next()' call karna zaroori hai
+  req.io = io;
+  next(); // Agle step par jaane ke liye 'next()' call karna zaroori hai
 });
 
 app.use('/api/auth', authRoutes)
 app.use('/api/attendance', attendanceRoutes)
 app.use('/api/users', userRoutes)
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 
 app.get('/', () => {
-    console.log("hello")
+  console.log("hello")
 })
 server.listen(process.env.PORT || 3000, () => {
-    console.log(`listen port ${process.env.PORT}`);
+  console.log(`listen port ${process.env.PORT}`);
 })
